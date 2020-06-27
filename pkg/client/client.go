9fe6f78453ad9e913cb449659b1ac00135e4e0fc
chinniehendrix <chinniehendrix@gmail.com>
2020-06-27 00:03:28 -0400

    refactor(client/main): move resty related types from client to main package

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,8 +2,6 @@ package client
 
 import (
 	"fmt"
-
-	"github.com/go-resty/resty/v2"
 )
 
 // HTTPClient ...
@@ -14,35 +12,6 @@ type HTTPClient interface {
 // HTTPClientFactory ...
 type HTTPClientFactory interface {
 	Create() HTTPClient
-}
-
-// RestyClient ...
-type RestyClient struct {
-	client *resty.Client
-}
-
-// NewRestyClient ...
-func NewRestyClient() *RestyClient {
-	r := RestyClient{
-		client: resty.New(),
-	}
-	return &r
-}
-
-// Get ...
-func (r RestyClient) Get(url string) (int, []byte, error) {
-	resp, err := r.client.R().Get(url)
-	body := resp.Body()
-	return resp.StatusCode(), body, err
-}
-
-// RestyClientFactory ...
-type RestyClientFactory struct{}
-
-// Create ...
-func (f RestyClientFactory) Create() HTTPClient {
-	r := NewRestyClient()
-	return *r
 }
 
 // ZooClient ...
