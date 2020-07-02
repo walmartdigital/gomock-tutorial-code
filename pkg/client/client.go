8e29deec1d09bcdb57903fd54dec8c13ab45b31b
Andres Benavides <andres.benavides@walmart.com>
2020-07-02 12:07:21 -0400

    feat(Tests): refactor ReadMessage function

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -29,6 +29,23 @@ func NewZooClient(factory HTTPClientFactory) *ZooClient {
 
 // ReadMessage ...
 func (z *ZooClient) ReadMessage(animal string) string {
-	_, body, _ := z.client.Get(fmt.Sprintf("http://localhost:8080/%s", animal))
-	return string(body)
+	statusCode, body, err := z.client.Get(fmt.Sprintf("http://localhost:8080/%s", animal))
+
+	if err != nil {
+		return string("Hi there, the zoo is closed!")
+	}
+
+	var singularDictionary = map[string]string{
+		"elephants": "elephant",
+		"dogs":      "dog",
+	}
+
+	switch statusCode {
+	case 200:
+		return string(body)
+	case 404:
+		return fmt.Sprintf("Hi there, what is an %s!", singularDictionary[animal])
+	default:
+		return string(body)
+	}
 }
